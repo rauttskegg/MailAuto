e78a1619a7d0f5c696345062378cc18860908779
Anton Kadantsev <kadantsev.anton@yandex.ru>
2022-10-27 20:32:37 +0300

    Не отправляет

--- MailAuto/Program.cs
+++ MailAuto/Program.cs
@@ -3,6 +3,7 @@
 using MailKit.Net.Smtp;
 using MailKit;
 using MimeKit;
+using MailKit.Security;
 
 namespace MailAuto
 {
@@ -10,8 +11,28 @@ namespace MailAuto
 	{
 		static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello, World!");
 			var message = new MimeMessage ();
+			message.From.Add (new MailboxAddress ("Anton", "a.kadantsev@mtsr.krasnodar.ru"));
+			message.To.Add (new MailboxAddress ("Mrs. Chanandler Bong", "kadantsev.anton@yandex.ru"));
+			message.Subject = "Test";
+
+			message.Body = new TextPart ("plain") {
+				Text = @"Hey Chandler,
+				I just wanted to let you know that Monica and I were going to go play some paintball, you in?
+				-- Joey"
+			};
+
+			using (var client = new SmtpClient ()) {
+				client.CheckCertificateRevocation = false;
+				client.Connect ("m.krasnodar.ru", 465, SecureSocketOptions.Auto);
+
+				// Note: only needed if the SMTP server requires authentication
+				client.Authenticate ("a.kadantsev@mtsr.krasnodar.ru", "VSdfSdf43**dfs4");
+
+				client.Send (message);
+				client.Disconnect (true);
+			}
+			Console.ReadKey ();
 		}
 	}
 }
